7168b3ab73c3fd4f7995e77d8e297ae99dcc0bc9
juanMoleroD <juanldimeco@gmail.com>
2022-09-01 19:00:49 +0100

    added basic message to error page

--- client/src/containers/ErrorPage.js
+++ client/src/containers/ErrorPage.js
@@ -3,6 +3,8 @@ import React from "react";
 const ErrorPage = () => {
     return(
         <>
+            <h1>Error!</h1>
+            <p>There has been an error, go back home</p>
         </>
     )
 }
