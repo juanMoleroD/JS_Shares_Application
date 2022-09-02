7dd3390da28df787cd0be1dd880d390c534efffd
juanMoleroD <juanldimeco@gmail.com>
2022-09-02 11:30:32 +0100

    Added API details

---
+++ client/src/SharesService.js
@@ -0,0 +1,5 @@
+import {API_key} from "./API_key";
+
+const baseURL = "https://www.alphavantage.co/query?function=";
+const weeklyData = "TIME_SERIES_WEEKLY&symbol=";
+const API_authorization = "&apikey=" + API_key;
