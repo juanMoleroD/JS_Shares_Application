d23c0e7dae63d3e8aea96e32cd982a9e31a9d6ce
juanMoleroD <juanldimeco@gmail.com>
2022-09-02 14:07:55 +0100

    added some changes

--- client/src/SharesService.js
+++ client/src/SharesService.js
@@ -3,3 +3,9 @@ import {API_key} from "./API_key";
 const baseURL = "https://www.alphavantage.co/query?function=";
 const weeklyData = "TIME_SERIES_WEEKLY&symbol=";
 const API_authorization = "&apikey=" + API_key;
+
+
+export const getShares = () => {
+    return fetch(baseURL + weeklyData + "TSLA" + API_authorization)
+            .then(response => response.json())
+}
--- client/src/containers/Portfolio.js
+++ client/src/containers/Portfolio.js
@@ -1,58 +1,33 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import ListOfShares from "../components/ListOfShares";
+import {getShares} from "../SharesService"
 
 
 const Portfolio = () => {
 
-    const [shares, setShares] = useState([]);
+    const [shares, setShares] = useState("");
+    const [share, setShare] = useState("");
 
-    const getShares = () => {
-        setShares( [
-            {
-                shareName: "GOOG",
-                sharePurchasePrice: 110,
-                heldAmount: 2
-            },
-            {
-                shareName: "AAPL",
-                sharePurchasePrice: 120,
-                heldAmount: 1
-            },
-            {
-                shareName: "TSLA",
-                sharePurchasePrice: 150,
-                heldAmount: 3
-            }
-        ])
-    }
-
-
-    const addShares = (share) => {
-        const temp = shares.map(s=>s);
-        temp.push(share);
-        setShares(temp);
-    }
-
-    const removeShares = (id) =>{
-        const temp = shares.map(s=> s);
-        const indexToDel = temp.map(s => s._id).index(id);
-        console.log(indexToDel)
-
-        temp.splice(indexToDel, 1);
-        setShares(temp)
-    }
-
-
-    useState(() => {
-        getShares();
+    
+    useEffect(() => {
+        getShares()
+            .then(response => {
+                setShares(response)
+                setShare(response["Meta Data"]["2. Symbol"])
+            });
     }, [])
+    // useEffect(());
     
     return(
         <>
         <main>
-        
                 <h2>Portfolio of shares:</h2>
-           <ListOfShares shares={shares}/>
+           {/* <ListOfShares shares={shares}/> */}
+                {
+                    shares.length > 0 ? <h3>{shares["Meta Data"]["1. Information"]}</h3> : <h3>loading</h3>
+                }
+                
+                {/* <h3>{share}</h3> */}
 
             </main>
         </>
