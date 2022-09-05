ff609bcb484f626c0ac8ae0ca547529f44eafe59
Harpreet Dhanda <harpreet_h@hotmail.co.uk>
2022-09-05 17:02:18 +0100

    Flexing data and adding account details and further updated

--- client/src/containers/Account.js
+++ client/src/containers/Account.js
@@ -5,10 +5,10 @@ const Account = ()=> {
 
     return(
 
-        <div>
-        <section>
+        <div className="account">
+        <section className="details">
             <h2>
-                Account Holder Details
+                Personal Details
             </h2>
             <p>Name: Ben Robert</p>
             <p>Email: BenRoberts@codeclan.com</p>
@@ -16,6 +16,24 @@ const Account = ()=> {
     
           <p className="upgradebutton">Membership: Free <button className="upgradebutton"><a href="https://www.alphavantage.co/support/#"> Upgrade Membership</a></button></p>
          
+        </section>
+
+        <section className="details">
+        <h2>
+            Bank Details
+        </h2>
+        <p>Bank Name: Starling</p>
+        <p>Bank Account Number: 23-41-**-** <button> Change Account Number </button></p>
+        <p>Sort-Code: 80-75-60<button> Change Sort-Code </button></p>
+
+        </section>
+        <section className="details">
+            <h2>Subscription</h2>
+            <p>Email Newsletter <input type="checkbox"/></p>
+            <p>Text <input type="checkbox"/></p>
+            <p>Post <input type="checkbox"/></p>
+            <p><button> Unsubscribe</button></p>
+
         </section>
 
         </div>
