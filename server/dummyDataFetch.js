4e9710dbcaf53eab20da08a341043c25b9284114
juanMoleroD <juanldimeco@gmail.com>
2022-09-06 11:05:56 +0100

    completed the functionality in the back end, created a file to fetch data from the API and store it, updated the get method to work with the symbol, updated the frontend to toggle between dev and normal API.

--- server/dummyDataFetch.js
+++ server/dummyDataFetch.js
@@ -1,18 +1,22 @@
 const API_Key = "J2DXV9A72P8LXA9R";
 const Weekly_series = "function=TIME_SERIES_WEEKLY"
 
-const shares = [
-    "IBM", "GOOG", "APPL", "TSLA", "AMZN", 
-    "MSFT", "META", "ORCL", "ADBE", "CSCO",
+const shares1 = [
+    "IBM", "GOOG", "AAPL", "TSLA", "AMZN"
+]
+const shares2 = [
+    "MSFT", "META", "ORCL", "ADBE", "CSCO"
+]
+const shares3 = [
     "PYPL", "NFLX", "SONY", "ABNB", "UBER"
 ]
 
 
-const addShareData = (timeSeries, shareName) => {
-    fetch(`https://www.alphavantage.co/query?${timeSeries}&symbol=${shareName}&apikey=${API_Key}`)
+const addShareData = (shareName) => {
+    fetch(`https://www.alphavantage.co/query?${Weekly_series}&symbol=${shareName}&apikey=${API_Key}`)
     .then(result => result.json())
     .then(response => {
-        fetch(`http://localhost:9000/api/dummyShares`, 
+        fetch(`http://localhost:9000/api/dummySharesWeekly`, 
             {
                 method: "POST",
                 body: JSON.stringify(response),
@@ -23,6 +27,24 @@ const addShareData = (timeSeries, shareName) => {
     
 }
 
-addShareData(Weekly_series, "IBM");
+// shares1.forEach(share => addShareData(share));
+// shares2.forEach(share => addShareData(share));
+// shares3.forEach(share => addShareData(share));
 
-//https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=J2DXV9A72P8LXA9R
+// addShareData("IBM");
+// addShareData("GOOG");
+// addShareData("AAPL");
+// addShareData("TSLA");
+// addShareData("AMZN");
+
+sleep = milliseconds => new Promise(resolve => setTimeout(resolve, milliseconds));
+
+getAllShares = async () => {
+    shares1.forEach(share => addShareData(share));
+    await sleep(61000); 
+    shares2.forEach(share => addShareData(share));
+    await sleep(61000); 
+    shares3.forEach(share => addShareData(share));	
+}
+
+getAllShares();
--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const createRouter = require('./helpers/create_router.js');
+const createDummyRouter = require("./helpers/dummy_router.js");
 const app = express();
 
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
-// app.use(express.urlencoded({limit: '50mb'}));
 
 const MongoClient = require("mongodb").MongoClient;
 MongoClient.connect("mongodb://127.0.0.1:27017", { useUnifiedTopology: true })
@@ -17,12 +17,10 @@ MongoClient.connect("mongodb://127.0.0.1:27017", { useUnifiedTopology: true })
         app.use("/api/portfolio", sharesRouter);
 
         const dummySharesWeeklyCollection = db.collection("dummySharesWeekly");
-        const dummySharesWeeklyRouter = createRouter(dummySharesWeeklyCollection);
-        app.use("/api/dummyShares/", dummySharesWeeklyRouter)
+        const dummySharesWeeklyRouter = createDummyRouter(dummySharesWeeklyCollection);
+        app.use("/api/dummySharesWeekly", dummySharesWeeklyRouter)
     })
     .catch(console.error);
-
-
 
 app.listen(9000, function() {
     console.log(`Listening to port ${this.address().port}`);
